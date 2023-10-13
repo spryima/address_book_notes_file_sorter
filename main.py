de883f74d4af9003e9c1e3641a5defb9f0b375c9
Sergii Pryima <spryima@gmail.com>
2023-10-13 17:31:38 +0300

    basic structer for main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from classes.uiclasses import ConsoleUserInterface    
+from mainfunc import parser
+
+
 def main():
-    ...
+    ui = ConsoleUserInterface()
+    ui.show_start_message()
+
+
+    while True:
+        message = ui.ask_question_input("   >>> ")
+        cmd, data = parser(message)
+        cmd(ui, *data)
+
 
 if __name__ == "__main__":
     main()
