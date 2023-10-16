e8b7dd32d98a4e13189bd08ce1d74c88a099cc54
Sergii Pryima <spryima@gmail.com>
2023-10-16 08:52:12 +0300

    alternative version

--- classes/abclasses.py
+++ classes/abclasses.py
@@ -1,88 +1,116 @@
 from collections import UserDict
-from datetime import datetime
 
 
 class Contact():
-    def __init__(self, name, surname='', phone='', birthday=None, email=''):
-        self.name = name
+    def __init__(self, surname):
         self.surname = surname
+        self.name = ''
         self.phones = []
-        self.birthday = None
-        self.email = email
-        if phone:
-            self.phones.append(phone)
+        self.birthday = ''
+        self.email = ''
+
+    def add_name(self, name):
+        if name:
+            self.name = name
+
+    def add_phone(self, phones):
+        if phones:
+            for phone in phones:
+                self.phones.append(phone)
+
+    def add_email(self, email):
+        if email:
+            self.email = email
+
+    def add_birthday(self, birthday):
         if birthday:
-            try:
-                self.birthday = datetime.strptime(birthday, "%d.%m.%Y").date()
-            except ValueError:
-                raise ValueError("Invalid birthday format (should be 'DD.MM.YYYY')")
-        
-        
-        def read_surname(self):
-            pass
+            self.birthday = birthday    
 
+    def update_surname(self, new_surname):
+            self.surname = new_surname
 
-        def update_surname(self, value: str):
-            pass
+    def update_name(self, new_name):
+        if new_name:  
+            self.name = new_name
 
+    def update_phone(self, *_):
+        pass
     
-        def read_birthday(self):
-            pass
-
-
-        def update_birthday(self, value: str):
-            pass
-
+    def update_birthday(self, new_birthday):
+        if new_birthday:
+            self.birthday = new_birthday
     
-        def read_email(self):
-            pass
+    def update_email(self, new_email):
+        if new_email:
+            self.email = new_email
 
+    def read_surname(self):
+        pass
 
-        def update_email(self, value: str):
-            pass
+    def read_birthday(self):
+        pass
 
+    def read_email(self):
+        pass
+
+    def read_phones(self):
+        pass
+
+    def delete_phone(self, value: str):
+        pass
+
+    def __repr__(self) -> str:
+        return "Surname: {:<10}  Name: {:<10}  Phone: {:<15}  Email: {:<15}  Birthday: {}".format(
+            self.surname,
+            self.name,
+            ", ".join(phone for phone in self.phones),
+            self.email,
+            self.birthday,
+        )
     
-        def add_phone(self, value: str):
-            pass
-
-
-        def read_phones(self):
-            pass
-
-
-        def update_phone(self, current_phone: str, new_phone: str):
-            pass
-
-    
-        def delete_phone(self, value: str):
-            pass
-    
-
 
 class AddressBook(UserDict):
-    
-    def add_contact(self, contact: Contact):
-        self.data.update({contact.name:contact})
-    
+        
+    def add_contact(self, ui , contact: Contact):
+            self.data.update({contact.surname:contact})
+            ui.show_green_message('This command will guide you through creating new contact:\n')
+            ui.show_message(f'Surname: {contact.surname}')
+            contact.add_name(ui.user_input('Name [Enter to skip]: '))
+            contact.add_phone(ui.user_input('Phones space-separate [Enter to skip]: ').split())
+            contact.add_birthday(ui.user_input('Birthday [Enter to skip]: '))
+            contact.add_email(ui.user_input('Email [Enter to skip]: '))
+        
 
     def read_contact(self, name: str):
         pass
 
 
-    def update_contact(self, contact: Contact):
-        pass
+    def update_contact(self, ui, contact: Contact):
+        ui.show_green_message('This command will guide you through updating contact:\n')
+        new_surname = ui.user_input(f'Surname: {contact.surname} [Enter to skip]: ') 
+        if new_surname in self.data:
+            ui.show_red_message(f'{new_surname} already exists ')
+            return
+        elif new_surname:
+            self.data[new_surname] = contact
+            del self.data[contact.surname]
+            contact.update_surname(new_surname)      
+        contact.update_name(ui.user_input(f'Name: {contact.name} [Enter to skip]: '))
+        contact.update_phone(ui.user_input(f'Phones {contact.phones} [Enter to skip]: ').split())
+        contact.update_birthday(ui.user_input(f'Birthday: {contact.birthday} [Enter to skip]: '))
+        contact.update_email(ui.user_input(f'Email: {contact.email} [Enter to skip]: '))
 
-    
+
     def delete_contact(self, name):
         if name in self.data:
             del self.data[name]
-    
-    
+
+
     def show_contacts(self):
         pass
 
-    
-    
+
+
 
             
       
--- main.py
+++ main.py
@@ -1,20 +1,121 @@
 from classes.uiclasses import ConsoleUserInterface    
-from classes.abclasses import AddressBook
-from mainfunc import parser
+from classes.abclasses import AddressBook, Contact
 
 
-def main():
-    ui = ConsoleUserInterface()
-    
-    ab = AddressBook()
-    
+
+
+def main():    
     ui.show_start_message()
     
     while True:
-        message = ui.user_input("   >>> ")
-        cmd, data = parser(ui, message)
-        cmd(ui, *data)
+        message = ui.user_input(f'\033[94m >>> \033[0m')
+        cmd, data = parser(message)
+        cmd(*data)
+
+
+def add_command(surname):
+     if surname in ab:
+         ui.show_red_message(f'Contact with surname {surname} already exists')
+     else:
+         ab.add_contact(ui, Contact(surname))
+
+def change_command(surname):
+     if surname in ab:
+        ab.update_contact(ui, ab[surname])         
+     else:
+        ui.show_red_message(f'No contacts with surname {surname}')
+
+def find_command(text):
+    ui.show_message("\n".join(str(contact) for contact in ab.values() if text in str(contact)))
+
+
+def delete_all_command():
+    ...
+
+
+def delete_command():
+    ...
+
+
+def help_command(*_):
+    ui.show_help()
+
+
+def unknown_command():
+    ui.show_red_message('Unknown command')
+
+
+def show_all_command(*_):
+    ui.clear_screen()
+    for contact in ab.values():
+            ui.show_message(contact)
+
+
+def sort_command():
+    ...
+
+
+def exit_command(*_):
+    ui.show_green_message(f"\nGood bye!\n\n")
+    exit()
+
+
+
+
+def parser(text):
+    for cmd, kwds in CMD_LIST.items():
+        for kwd in kwds:
+            if text.strip().lower().split()[0] == kwd:
+                return cmd, text[len(kwd):].strip().split(" ")
+    
+    closest_cmd = levenshtein_distance(text.strip().split()[0].lower())
+    if closest_cmd:
+       return parser(text.replace(text.strip().split()[0], closest_cmd, 1)) 
+
+    return unknown_command, []
+
+
+def levenshtein_distance(str_to_check):
+    distance = len(str_to_check)
+    possible_cmd = None
+    for kwds in CMD_LIST.values():
+        for cmd in kwds:
+            m, n = len(str_to_check), len(cmd)
+            dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+            for i in range(m+1):
+                dp[i][0] = i
+            for j in range(n+1):
+                dp[0][j] = j
+            for i in range(1, m+1):
+                for j in range(1, n+1):
+                    substitution_cost = 0 if str_to_check[i-1] == cmd[j-1] else 1
+                    dp[i][j] = min(dp[i-1][j] + 1,
+                                dp[i][j-1] + 1,
+                                dp[i-1][j-1] + substitution_cost) 
+            if dp[m][n] < distance:
+                distance = dp[m][n]
+                possible_cmd = cmd
+    if distance < len(str_to_check):
+        ui.show_message(f'Did you mean "{possible_cmd} "?')
+        if ui.user_input('Y/n:  ').lower() in ('y', 'yes'):
+            return possible_cmd
+
+
+CMD_LIST = {
+    add_command: ("add", "+"),
+    find_command: ("find",),
+    change_command: ("change",),
+    show_all_command: ("show all", "show"),
+    delete_all_command: ("delete all", "remove all"),
+    delete_command: ("delete", "del", "remove"),
+    help_command: ("help", "h", "?"),
+    exit_command: ("exit", "quit", "goodbye",  "."),
+    sort_command: ("sort"),
+}
 
 
 if __name__ == "__main__":
+    
+    ui = ConsoleUserInterface()
+    ab = AddressBook()
     main()
