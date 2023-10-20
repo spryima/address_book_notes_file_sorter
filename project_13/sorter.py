792842aef938ab4e5b809ef3873523949d61d60a
Sergii Pryima <spryima@gmail.com>
2023-10-20 14:15:21 +0300

    sorter alternative

--- project_13/sorter.py
+++ project_13/sorter.py
@@ -1,157 +1,130 @@
-import shutil
-import argparse
-import re
+import sys
 from pathlib import Path
+import shutil
 
-#EXTENSIONS
-EXTENSIONS = {
-"images" : ('.jpeg', '.png', '.jpg', '.svg', '.webp', '.JPG', '.PNG'),
-"videos" : ('.avi', '.mp4', '.mov', '.mkv','.MP4'),
-"docs" : ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx', '.html'),
-"music" : ('.mp3', '.ogg', '.wav', '.amr'),
-"apps" : ('.exe', '.dmg', '.pkg'),
-"archs" : ('.zip', '.gz', '.tar') }
-
-#EXTENSIONS WHICH WERE SORTED
-extension_set = set()
-
-#SORTED FILES
-files_dict = {
-    "images":[],
-    "videos":[],
-    "docs":[],
-    "music":[],
-    "apps":[],
-    "archives":[],
-    "another":[],
+categories = {
+    'Images': ['.jpeg', '.png', '.jpg', '.svg'],
+    'Videos': ['.avi', '.mp4', '.mov', '.mkv'],
+    'Music': ['.mp3', '.ogg', '.wav', '.amr'],
+    'Archives': ['.zip', '.tar', '.rar', '.gz'],
+    'Documents': ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx', 'xls'],
+    'Other': []
 }
 
-#DIRS
-dirs = ["images", "videos", "docs", "music", "apps", "archives", "bin", "another"]
 
-#TRANSLITION
-TRANS = {}
-for c, l in zip("абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ", ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
-               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")):
-    TRANS[ord(c)] = l
-    TRANS[ord(c.upper())] = l.upper()
-
-#NORMALIZING TEXT
 def normalize(line: str) -> str:
-    separate=line.rsplit(".", 1)
-    line = separate[0]
-    line = line.translate(TRANS)
-    line = re.sub(r"[^a-zA-Z0-9]", "_", line)
-    try:
-        return str(line+"."+separate[1])
-    except IndexError:
-        return(line)
+    cyr_to_lat_dict = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h',
+        'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'e',
+        'ж': 'zh', 'з': 'z', 'и': 'y', 'і': 'i',
+        'ї': 'i', 'й': 'i', 'к': 'k', 'л': 'l',
+        'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+        'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+        'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch',
+        'ш': 'sh', 'щ': 'shch', 'ю': 'u', 'я': 'ya',
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'H', 
+        'Ґ': 'G', 'Д': 'D', 'Е': 'E', 'Є': 'E', 
+        'Ж': 'ZH', 'З': 'Z', 'И': 'Y', 'І': 'I', 
+        'Ї': 'I', 'Й': 'I', 'К': 'K', 'Л': 'L', 
+        'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 
+        'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 
+        'Ф': 'F', 'Х': 'KH', 'Ц': 'TS', 'Ч': 'CH', 
+        'Ш': 'SH', 'Щ': 'SHCH', 'Ю': 'U', 'Я': 'YA', 
+    }
+    for cyr, lat in cyr_to_lat_dict.items():
+        if cyr in line:
+            line = line.replace(cyr, lat)
 
-#CUTTING TEXT TO 20 SYMBOLS
-def cut(ins: str) -> str:
-    if len(ins)>20:
-        ins = ins[:17] +"..."
-    return ins
+    for i in range(len(line)):
+        if line[i].isdigit() or line[i].isalpha():
+            continue
+        line = line.replace(line[i], '_')
+    return line
 
-#MOVING EMPTY FOLDERS TO BIN
-def move_empty_folders(path: Path) -> None:
-    for file in Path(path).iterdir():
-        if file.name not in set(dirs):
-            if Path.is_dir(file):
-                if file.name not in set(dirs):
-                    target_dir = Path(path, "bin")
-                    target_dir.mkdir(parents=False, exist_ok=True)
-                    try:
-                        move(target_dir, file)
-                    except shutil.Error as e:
-                        print(e)
-         
-#NORMALIZING FILE'S NAMES
-def rename_all(path:Path) -> None:
-    for file in Path(path).iterdir():
-        if Path.is_dir(file):
-            rename_all(file)
-        else:
-            try:
-                Path(file).rename(Path(file.parent, normalize(file.name)))
-            except NotADirectoryError:
-                ...
-            #ALREADY RENAMED
-            
+def report(root_dir):
+    print('Список файлів в кожній категорії:')
+    for item in root_dir.iterdir():
+        if item.is_dir():
+            print(item.stem)
+            for file in item.iterdir():
+                print(f'    {file.stem}{file.suffix}')
+
+    known_ext = set()
+    unknown_ext = set()
+    all_ext = []
+
+    for values_list in categories.values():
+        all_ext.extend(values_list)
+     
+    for item in root_dir.iterdir():
+        if item.is_dir():
+            for file in item.iterdir():
+                if file.suffix.lower() in all_ext:                 
+                    known_ext.add(file.suffix.lower())
+                else:
+                    unknown_ext.add(file.suffix.lower())
+
+    print('')
+    print('Перелік усіх відомих скрипту розширень, які зустрічаються в цільовій теці:')
+    print(' '.join(map(str, known_ext)), end='\n\n')
+    print('Перелік всіх розширень, які скрипту невідомі:')
+    print(' '.join(map(str, unknown_ext)), end='\n\n')
+
+
+def unpack(file: Path, destination_dir: Path) ->None:
+    destination_dir = destination_dir.parent.joinpath(f'{destination_dir.stem}',)
+    if destination_dir.exists():
+        destination_dir = get_unique_name(destination_dir)        
+    shutil.unpack_archive(file.resolve(), destination_dir)
+    file.unlink()
+
+
 def get_unique_name(file: Path) -> Path:
     new_id = 1
     while file.exists():
-        if new_id > 1:
-            file = file.parent.joinpath(f'{file.stem.rsplit("_", 1)[0]}_{new_id}{file.suffix}')
-        else:
-            file = file.parent.joinpath(f'{file.stem}_{new_id}{file.suffix}')
-        new_id +=1
+        file = file.parent.joinpath(f'{file.stem}_{new_id}{file.suffix}')
     return file
-            
-def move(move_to_dir, file):
-    future_file = Path(move_to_dir, file.name)
-    if future_file.exists():
-        future_file = get_unique_name(future_file)
+
+
+def move_file(file: Path, root_dir: Path, category: str) -> None:
+    destination_dir = root_dir.joinpath(category)
+    if not destination_dir.exists():
+        Path.mkdir(destination_dir)
+    new_file_dist = destination_dir.joinpath(f'{normalize(file.stem)}{file.suffix}')
+    if category == 'Archives':
+        unpack(file, new_file_dist)
+        return
+    if new_file_dist.exists():
+        new_file_dist = get_unique_name(new_file_dist)
+    file.replace(new_file_dist)
+
+
+def categorize(file: Path) -> str:
+    for category, cat_ext in categories.items():
+        if file.suffix.lower() in cat_ext:
+            return category
+    return 'Other'
+
+
+def sorter(path: Path, root_dir: Path) -> None:
+    for item in path.iterdir():
+        if item.is_file():
+            file_category = categorize(item)                            
+            move_file(item, root_dir, file_category)
+        if (item.is_dir() and item.stem not in categories) or (item.is_dir() and item.parent!=root_dir):
+            sorter(item, root_dir)
+            if not any(item.iterdir()):
+                item.rmdir()
+
+
+def clear(file):
     try:
-        shutil.move(file, future_file)
-    except FileNotFoundError:
-        ... 
-
-#SORTING
-def sort(path, path_ = None):
-    path_ = path
-    for file in Path(path_).iterdir():
-        extension_set.add((file.suffix))
-        for list, dir in zip(EXTENSIONS.values(), dirs):
-            if file.name.endswith(list):
-                files_dict[dir].append(file.name)
-                target_dir = Path(path, dir)
-                target_dir.mkdir(parents=False, exist_ok=True)
-                move(target_dir, file)
-            #FINDING AND UNARCHIVATING ZIPS
-            elif file.name.endswith(EXTENSIONS["archs"]):
-                files_dict["archives"].append(file.name)
-                target_dir = Path(path, "archives/" + normalize((file.name).rsplit(".",1)[0]))
-                target_dir.mkdir(parents=False, exist_ok=True)
-                try:
-                    shutil.unpack_archive(file, target_dir)
-                except shutil.ReadError:
-                    ...
-            elif Path(file).is_dir() and file.name not in dirs:
-                sort(path, path_ + "/" + file.name)
-                
-    #COLLECTING ALL UNKNOWN EXTENSION FILES
-    for file in Path(path).iterdir():
-        if Path(file).is_file():
-            files_dict["another"].append(file.name)
-            target_dir = Path(path, "another")
-            target_dir.mkdir(parents=False, exist_ok=True)
-            move(target_dir, file)
-
-def clear(path):
-
-    sort(path)
-    rename_all(path)
-    move_empty_folders(path)
-    
-    #PRINTING SORTED FILES
-    if extension_set != {""}:
-        for i, _ in files_dict.items():   
-            if _:
-                print("|{:^25}|".format(""))
-                print("|{:^25}|".format(i))
-                for i in _:
-                    print("|{:^25}|".format(cut(i)))
-                print("|{:^25}|".format("-----------"))
-        print("|{:^25}|".format(""))
-        
-        all_extensions = set()
-        for extensions_tuple in EXTENSIONS.values():
-            all_extensions.update(extensions_tuple)
-
-        unknown_ext = extension_set - all_extensions
-        print(f"unknown by script extensions: {unknown_ext}")
-        print(f"previously known by script extensions: {extension_set - unknown_ext}")
-    else:
-        print("No sort was performed")
-        
+        file = Path(file)
+        root_dir = Path(file).resolve()
+    except IndexError:
+        return "No path to folder"
+    if not root_dir.exists():
+        return 'Folder does not exists'
+  
+    sorter(file, root_dir)
+    report(root_dir)
